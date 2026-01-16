c53d42ab0220f3a191c3530d68e40545fa08ad69
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-01-16 15:29:59 +0100

    Completed task 1

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,20 @@
-# Write your code here
+echo "Creating project..."
+mkdir project
+cd project && git init
+mkdir -p resources src/database src/profile && touch README.md settings.conf "resources/family picture.jpg" resources/icon.png resources/logo.png src/program.java
+git add . && git commit -m "initial commit"
+sleep 3
+echo "Setup project..."
+echo "Welcome to my project" > README.md
+rmdir src/profile
+rm "resources/family picture.jpg"
+git add . && git commit -m "Add info to README.md"
+sleep 3
+ls -la Resources
+echo "Setup javascript..."
+mv src/program.java src/program.js
+echo 'console.log("JavaScript works!");' > src/program.js
+node src/program.js
+git add . && git commit -m "Change program.java to program.js"
+ls -la ~
+echo "All done!"
